2bdb0e845604842ad6593357fea1acaa1bd7c0cd
greyia <info@thun.ws>
2016-03-10 19:39:21 +0900

    cleanup

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    @staticmethod
+    def init_app(app):
+        pass
+
+class TestingConfig(Config):
+    TESTING = True
+
+class DevelopConfig(Config):
+    DEBUG = True
+
+class MasterConfig(Config):
+    DEBUG = False
+
+
+config = {
+        "default": MasterConfig,
+        "develop": DevelopConfig,
+        "testing": TestingConfig,
+        "master": MasterConfig,
+        "ci": TestingConfig
+        }
